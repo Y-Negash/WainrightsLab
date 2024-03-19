fe490d3cd2743e2da821c05b753942a9b4413f5d
Yeabsira Negash <n.yeabsira.a@gmail.com>
2024-03-19 15:44:26 +0000

    create elements for the unordered list

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 // global variable
 let wainwrights;
 
+//grabs
+const container = document.querySelector("#wainwrightsContainer");
+const list = document.querySelector("#wainwrights-list");
+
 //get data for all wainrights
 const getAllWainrights = async () => {
     const response = await fetch("https://raw.githubusercontent.com/annahndr/annahndr.github.io/master/wainwrights_data/wainwrights.json");
@@ -8,6 +12,17 @@ const getAllWainrights = async () => {
     return wainwrights = data;//assign data to global variable
 }
 
+//create elements for property of every wainright
+const wainrightName = document.createElement("h2")
+const wainrightHeight = document.createElement("h2")
+const wainrightArea = document.createElement("h2")
+const wainrightAbout = document.createElement("h2")
+
+// adds it to unordered list
+list.appendChild(wainrightName);
+list.appendChild(wainrightHeight);
+list.appendChild(wainrightArea);
+list.appendChild(wainrightAbout);
 
 
 
