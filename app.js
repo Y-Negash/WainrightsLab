0b0d1aa679f9d6099f50c30b7c213fb7581c9c03
Yeabsira Negash <n.yeabsira.a@gmail.com>
2024-03-19 19:43:37 +0000

    creates filter method and adds comments

--- app.js
+++ app.js
@@ -1,40 +1,54 @@
 //grabs
-const container = document.querySelector("#wainwrightsContainer");
-const list = document.querySelector("#wainwrights-list"); //global variable
+const list = document.querySelector("#wainwrights-list"); 
+const wainrightForm = document.querySelector("#wainright-form");
+let totalWainrights;
+
+function appendElements(filteredWainrights){filteredWainrights.forEach((hill) => {
+    //function that creates new elements about each Wainright
+    
+    const wainrightName = document.createElement("h2");
+        const wainrightHeight = document.createElement("h3");
+        const wainrightArea = document.createElement("p");
+        const wainrightAbout = document.createElement("p");
+        
+        //fills new elements with properties of the hill
+        wainrightName.innerText = hill.name;
+        wainrightHeight.innerText = hill.heightMetres;
+        wainrightArea.innerText = hill.area.areaName;
+        wainrightAbout.innerText = hill.area.about;
+        
+        // adds to unordered list with the new properties...
+        list.appendChild(wainrightName);
+        list.appendChild(wainrightHeight);
+        list.appendChild(wainrightArea);
+        list.appendChild(wainrightAbout);
+
+    });
+
+}
 
 //get data for all wainrights
 const getAllWainrights = async () => {
-    const response = await fetch("https://raw.githubusercontent.com/annahndr/annahndr.github.io/master/wainwrights_data/wainwrights.json");
-    const data = await response.json();
-    // console.log(data);
-    data.forEach((element) => createWainright(element))
     
-    // createWainrights(wainwrights);
+    const response = await fetch("https://raw.githubusercontent.com/annahndr/annahndr.github.io/master/wainwrights_data/wainwrights.json");
+    totalWainrights = await response.json();
+    appendElements(totalWainrights);
+    
+// filter wainrights by inputed value in form 
+    wainrightForm.addEventListener("submit", (event) => {
+        event.preventDefault();
+        
+        const wantedWainright = event.target["input"].value;
+    if(wantedWainright !== ""){
+        const filteredWainrights = totalWainrights.filter(hill => {
+            return hill.name.toLowerCase().includes(wantedWainright.toLowerCase());
+        })
+        console.log(filteredWainrights);
+        list.innerText = "";
+        appendElements(filteredWainrights);
+    }
+    })
 }
-
-//function that creates new elements about each Wainright
-const createWainright = (hill) => {
-    const wainrightName = document.createElement("h2");
-    const wainrightHeight = document.createElement("h3");
-    const wainrightArea = document.createElement("p");
-    const wainrightAbout = document.createElement("p");
-
-//fills new elements with properties of the hill
-    wainrightName.innerText = hill.name;
-    wainrightHeight.innerText = hill.heightMetres;
-    wainrightName.innerText = hill.area.areaName;
-    wainrightAbout.innerText = hill.area.about;
-
-// adds new list to unordered list with the new properties...
-    list.appendChild(wainrightName);
-    list.appendChild(wainrightHeight);
-    list.appendChild(wainrightArea);
-    list.appendChild(wainrightAbout);
-
-}
-
-//function that listens to form
-
-
 //call function
 getAllWainrights();
+
