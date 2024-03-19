e80c24edb67ccfd63823940214d6f530ea04d5e8
Yeabsira Negash <n.yeabsira.a@gmail.com>
2024-03-19 15:19:06 +0000

    creates getAllWainrights function

--- app.js
+++ app.js
@@ -1,3 +1,15 @@
-const getAllWainrights = () => {
+// global variable
+let wainwrights;
 
+//get data for all wainrights
+const getAllWainrights = async () => {
+    const response = await fetch("https://raw.githubusercontent.com/annahndr/annahndr.github.io/master/wainwrights_data/wainwrights.json");
+    const data = await response.json();
+    return wainwrights = data;//assign data to global variable
 }
+
+
+
+
+//call function
+getAllWainrights();
