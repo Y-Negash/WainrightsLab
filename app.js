4d8bcb8839b2bae319a760b6696cdbde3993e0be
Yeabsira Negash <n.yeabsira.a@gmail.com>
2024-03-19 16:47:35 +0000

    fix createWainrights function and use foreach instead

--- app.js
+++ app.js
@@ -1,29 +1,19 @@
-// global variable
-let wainwrights;
-
 //grabs
 const container = document.querySelector("#wainwrightsContainer");
-const mainList = document.querySelector("#wainwrights-list");
+const list = document.querySelector("#wainwrights-list"); //global variable
 
 //get data for all wainrights
 const getAllWainrights = async () => {
     const response = await fetch("https://raw.githubusercontent.com/annahndr/annahndr.github.io/master/wainwrights_data/wainwrights.json");
     const data = await response.json();
-    wainwrights = data;//assign data to global variable
-    mapWainrights(wainwrights);
-}
-
-//loop through wainright and add to list
-// wainwrights.forEach((element) => newWainright(element))
-const mapWainrights = (allWainrights) => {
-    for(let hill of allWainrights){
-        createWainright(hill);
-    }
+    console.log(data);
+    data.forEach((element) => createWainright(element))
+    
+    // createWainrights(wainwrights);
 }
 
 //function that creates new elements about each Wainright
 const createWainright = (hill) => {
-    const list = document.createElement("li");
     const wainrightName = document.createElement("h2");
     const wainrightHeight = document.createElement("h3");
     const wainrightArea = document.createElement("p");
@@ -35,13 +25,12 @@ const createWainright = (hill) => {
     wainrightName.innerText = hill.area.areaName;
     wainrightAbout.innerText = hill.area.about;
 
-// adds it to unordered list
+// adds new list to unordered list with the new properties...
     list.appendChild(wainrightName);
     list.appendChild(wainrightHeight);
     list.appendChild(wainrightArea);
     list.appendChild(wainrightAbout);
 
-    mainList.appendChild(list);
 }
 
 //call function
