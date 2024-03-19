4a23c2a8395e10fa5b67b7103dfd52790bf16898
Yeabsira Negash <n.yeabsira.a@gmail.com>
2024-03-19 16:22:26 +0000

    makes createWainrights funtion

--- app.js
+++ app.js
@@ -3,28 +3,46 @@ let wainwrights;
 
 //grabs
 const container = document.querySelector("#wainwrightsContainer");
-const list = document.querySelector("#wainwrights-list");
+const mainList = document.querySelector("#wainwrights-list");
 
 //get data for all wainrights
 const getAllWainrights = async () => {
     const response = await fetch("https://raw.githubusercontent.com/annahndr/annahndr.github.io/master/wainwrights_data/wainwrights.json");
     const data = await response.json();
-    return wainwrights = data;//assign data to global variable
+    wainwrights = data;//assign data to global variable
+    mapWainrights(wainwrights);
 }
 
-//create elements for property of every wainright
-const wainrightName = document.createElement("h2")
-const wainrightHeight = document.createElement("h2")
-const wainrightArea = document.createElement("h2")
-const wainrightAbout = document.createElement("h2")
+//loop through wainright and add to list
+// wainwrights.forEach((element) => newWainright(element))
+const mapWainrights = (allWainrights) => {
+    for(let hill of allWainrights){
+        createWainright(hill);
+    }
+}
+
+//function that creates new elements about each Wainright
+const createWainright = (hill) => {
+    const list = document.createElement("li");
+    const wainrightName = document.createElement("h2");
+    const wainrightHeight = document.createElement("h3");
+    const wainrightArea = document.createElement("p");
+    const wainrightAbout = document.createElement("p");
+
+//fills new elements with properties of the hill
+    wainrightName.innerText = hill.name;
+    wainrightHeight.innerText = hill.heightMetres;
+    wainrightName.innerText = hill.area.areaName;
+    wainrightAbout.innerText = hill.area.about;
 
 // adds it to unordered list
-list.appendChild(wainrightName);
-list.appendChild(wainrightHeight);
-list.appendChild(wainrightArea);
-list.appendChild(wainrightAbout);
+    list.appendChild(wainrightName);
+    list.appendChild(wainrightHeight);
+    list.appendChild(wainrightArea);
+    list.appendChild(wainrightAbout);
 
-
+    mainList.appendChild(list);
+}
 
 //call function
 getAllWainrights();
